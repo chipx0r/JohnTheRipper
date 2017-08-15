2622af91dbc36f2b5e2cafe4e79e652dfb6acd4a
Dhiru Kholia <dhiru.kholia@gmail.com>
2017-08-15 18:53:08 +0530

    Release macOS builds on GitHub

--- .travis/check.sh
+++ .travis/check.sh
@@ -4,10 +4,35 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     # brew install --force openssl
     cd src
 
-    ./configure CPPFLAGS="-I/usr/local/opt/openssl/include" LDFLAGS="-L/usr/local/opt/openssl/lib"
+    # Build with AVX
+    ./configure --disable-native-tests CPPFLAGS='-mavx -I/usr/local/opt/openssl/include -DCPU_FALLBACK -DCPU_FALLBACK_BINARY="\"john-sse4.1\""' LDFLAGS="-L/usr/local/opt/openssl/lib"
     make -sj4
+    mv ../run/john ../run/john-avx
+    make clean; make distclean
 
-    ../.travis/test.sh
+    # Build with AVX2
+    ./configure --disable-native-tests CPPFLAGS='-mavx2 -I/usr/local/opt/openssl/include -DCPU_FALLBACK -DCPU_FALLBACK_BINARY="\"john-avx\""' LDFLAGS="-L/usr/local/opt/openssl/lib"
+    make -sj4
+    mv ../run/john ../run/john-avx2
+    make clean; make distclean
+
+    # Build with SSE4.1 ("widely" compatible binary)
+    ./configure --disable-native-tests CPPFLAGS='-I/usr/local/opt/openssl/include' LDFLAGS="-L/usr/local/opt/openssl/lib"
+    make -sj4
+    mv ../run/john ../run/john-sse4.1
+
+    mv ../run/john-avx2 ../run/john  # call the most feature-rich binary 'john'
+
+    # ./configure CPPFLAGS="-I/usr/local/opt/openssl/include" LDFLAGS="-L/usr/local/opt/openssl/lib"
+    # make -sj4
+
+    # ../.travis/test.sh
+
+    cd ..
+    rm -rf run/ztex
+
+    echo "These macOS Sierra builds require OpenSSL and GMP. To install these dependencies, run 'brew install --force openssl gmp' command." > README-macOS.txt
+    zip -r JtR-macOS.zip run/ doc/ README.md README README-jumbo README-macOS.txt
 
 elif [[ -z "$TEST" ]]; then
     cd src
